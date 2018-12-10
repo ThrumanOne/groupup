50f53982c154e46f528f74053c6bbffa21ba1ef5
Thruman <1006556989@qq.com>
2018-12-10 13:39:18 +0800

    添加测试

---
+++ multiDatasoureManagerDemo/src/main/java/test/SpringTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 类说明
+ * <p>
+ *
+ * @author niexiang
+ * @version 1.0.0
+ * @date 2018/12/10
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class SpringTest {
+}
